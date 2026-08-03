0bcfd30e06d07d942425c6a71ce34391e8c2424b
PicksN1k <csjohnson2838@gmail.com>
2026-08-03 16:55:49 -0400

    modified:   3_React/src/ShowCoordinatorInfo.js

--- 3_React/src/ShowCoordinatorInfo.js
+++ 3_React/src/ShowCoordinatorInfo.js
@@ -1,21 +1,36 @@
 // File: ShowCoordinatorInfo.js
 
 export default function CoordinatorInfo({ coordinatorInfo, describe }) {
-  
+
   function handleCourseClick(event, id) {
     event.preventDefault();
     console.log("Handle Course", id);
     describe(id);
   }
 
-  if (coordinatorInfo)
-    return (
-      <div>       
+  if (!coordinatorInfo)
+    return null;
 
-        Code goes here
-          
-          
-      </div>
-    );
+  return (
+    <div>
 
+      <h3>Coordinator Information</h3>
+
+      <h4>
+        {coordinatorInfo.firstName} {coordinatorInfo.lastName}
+      </h4>
+
+      <ul>
+        {coordinatorInfo.courses.map(course => (
+          <li key={course._id}>
+            <a href="/" onClick={(e) => handleCourseClick(e, course._id)}>
+              {course._id}
+            </a>{" "}
+            {course.courseName}
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
 }
